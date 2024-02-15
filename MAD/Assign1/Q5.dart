ceed3ecb9ce2d5004a5a48182f716df08fc32b38
Sujay Gangan <118454203+SujayGangan@users.noreply.github.com>
2024-02-15 16:02:57 +0530

    Create Q5.dart

---
+++ MAD/Assign1/Q5.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  // 5. Write a program to find quotient and remainder of two integers.
+  int num1 = 19, num2 = 4;
+  int quotient = num1 ~/ num2;
+  print(quotient);
+  int remainder = num1 % num2;
+  print(remainder);
+}
