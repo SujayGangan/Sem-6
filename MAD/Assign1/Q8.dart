cf5e4160eb07fa0e59ff358d4112feff10db0dec
Sujay Gangan <118454203+SujayGangan@users.noreply.github.com>
2024-02-15 16:05:51 +0530

    Create Q8.dart

---
+++ MAD/Assign1/Q8.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  // 8. Write a Dart program to convert String to int
+  String string = '1234';
+  int integer = int.parse(string);
+  print('string to integer : $integer');
+}
