cea9f241241b206e1bedeccbaa95cd67b8da1c84
Sujay Gangan <118454203+SujayGangan@users.noreply.github.com>
2024-02-20 20:21:20 +0530

    Create Q6.dart

---
+++ MAD/Assign2/Q6.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  // 6.Write a dart program to generate multiplication tables of 5
+
+  int r=5;
+  print("5's table");
+  for(int i=1;i<=10;i++) {
+    int m=r*i;
+    print(" $r * $i = $m");
+  }
+}
