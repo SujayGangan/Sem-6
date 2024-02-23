d110fd1a643c915242ad987cf32f8bec9d29c5d3
Sujay Gangan <118454203+SujayGangan@users.noreply.github.com>
2024-02-23 19:16:42 +0530

    Update Q1.dart

--- MAD/Assign4/Q1.dart
+++ MAD/Assign4/Q1.dart
@@ -1,19 +1,11 @@
 import 'dart:io';
 
 void main(List<String> args) {
-// 8. Write a function in Dart called calculateArea that calculates the area of a rectangle.
-//    It should take length and width as arguments, with a default value of 1 for both. Formula: length * width.
+  // 1. Create a list of names and print all names using list.
 
-  stdout.write("Enter length of rectangle : ");
-  int? length = int.parse(stdin.readLineSync()!);
+  // var list = ["Sujay", "Om", "Shreyas", "Pranav", "Aditya"];
+  // stdout.write(list);
 
-  stdout.write("Enter width of rectangle : ");
-  int? width = int.parse(stdin.readLineSync()!);
-
-  stdout.write("Area of rectangle is : ${calculateArea(length, width)}");
-}
-
-int? calculateArea(int? length, int? width) {
-  int? area = length! * width!;
-  return (area);
+  List<String> names = ["Sujay", "Zyed", "Shreyas", "Pranav", "Aditya"];
+  stdout.write(names);
 }
