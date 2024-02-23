49f2ec68d1ac119ad33a0bd828d8a58b9d90e4dc
Sujay Gangan <118454203+SujayGangan@users.noreply.github.com>
2024-02-23 19:18:09 +0530

    Create Q3.dart

---
+++ MAD/Assign4/Q3.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // 3. Create an empty list of type string called days. Use the add method to add names of 7 days and print all days.
+
+  List<String> days = [];
+  days.add("Sunday");
+  days.add("Monday");
+  days.add("Tuesday");
+  days.add("Wednesday");
+  days.add("Thusrsday");
+  days.add("Friday");
+  days.add("Saturday");
+  stdout.write(days);
+}
